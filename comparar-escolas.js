9f15920b2b1342c7a5f4ce21d18ba8469e850fdd
migueldinois <miguel.dinois@aluno.senai.br>
2026-03-13 14:37:09 -0300

    ADD: Fechar, arrumando validacao, comecando o relatorio

--- comparar-escolas.js
+++ comparar-escolas.js
@@ -52,27 +52,57 @@ const btnInstanciarEscolaB = document.querySelector('.unidadeB__instanciar');
 const btnAbrirEscolaA = document.querySelector('.unidadeA__abrir')
 const btnAbrirEscolaB = document.querySelector('.unidadeB__abrir')
 
+const btnFecharEscolaA = document.querySelector('.unidadeA__fechar')
+const btnFecharEscolaB = document.querySelector('.unidadeB__fechar')
+
+//  Campos mensagem
 const pMensagemB = document.querySelector('.unidadeB__mensagem');
 const pMensagemA = document.querySelector('.unidadeA__mensagem');
+
+//  Campos relatorio
+const campoVeredito = document.querySelector('.relatorio__veredito')
+const campoDetalhesA = document.querySelector('.relatorio__linha--a')
+const campoDetalhesB = document.querySelector('.relatorio__linha--B')
+
 
 
 /* ===== FUNÇÃO DE VALIDAÇÃO ===== */
 
+function validarCampos(unidade) {
+  if (unidade === 'A'){
+    if ((inputUnidadeACidade.value === '' || inputUnidadeACodigo.value === '' || inputUnidadeAAno.value === '' || inputUnidadeACursos.value === '')){
+      return false
+    } else {
+      return true
+    }
+  }
 
-
+  else if(unidade === 'B'){
+    if((inputUnidadeBCidade.value === '' || inputUnidadeBCodigo.value === '' || inputUnidadeBAno.value === '' || inputUnidadeBCursos.value === '')){
+      return false
+    } else {
+      return true
+    }
+  }
+}
 /* ===== INSTANCIAR ESCOLA A ===== */
 
 btnInstanciarEscolaA.addEventListener('click', () => {
-  if (inputUnidadeACidade.value === '' || inputUnidadeACodigo.value === '' || inputUnidadeAAno.value === '' || inputUnidadeACursos.value === '') {
-    pMensagemA.textContent = 'Preencha todos os campos para criar a escola.'
-    
-  } else {
+  
+  if (validarCampos('A') === true) {
+    console.log(validarCampos('A'))
     valorInputCidadeA = inputUnidadeACidade.value
     valorInputCodigoA = parseInt(inputUnidadeACodigo.value)
     valorInputAnoA = parseInt(inputUnidadeAAno.value)
     valorInputCursosA = inputUnidadeACursos.value
-    const novaEscolaA = new Senai(valorInputCidadeA, valorInputCodigoA, valorInputAnoA, valorInputCursosA)
+    const novaEscolaA = new Senai(valorInputCodigoA, valorInputCidadeA, valorInputAnoA, valorInputCursosA)
+    // Passando esse objeto para o escopo global
+    globalThis.novaEscolaA = novaEscolaA
     pMensagemA.textContent = `SENAI ${novaEscolaA.cidade} foi instanciada`
+
+  } else {
+    console.log(validarCampos('A'))
+    pMensagemA.textContent = 'Preencha todos os campos para criar a escola.'
   }
 
 })
@@ -81,16 +111,18 @@ btnInstanciarEscolaA.addEventListener('click', () => {
 /* ===== INSTANCIAR ESCOLA B ===== */
 
 btnInstanciarEscolaB.addEventListener('click', () => {
-  if (inputUnidadeBCidade.value === '' || inputUnidadeBCodigo.value === '' || inputUnidadeBAno.value === '' || inputUnidadeBCursos.value === '') {
-    pMensagemB.textContent = 'Preencha todos os campos para criar a escola.'
-    
-  } else {
+  if (validarCampos('B') === true) {
+    console.log(validarCampos('B'))
     valorInputCidadeB = inputUnidadeBCidade.value
     valorInputCodigoB = parseInt(inputUnidadeBCodigo.value)
     valorInputAnoB = parseInt(inputUnidadeBAno.value)
     valorInputCursosB = inputUnidadeBCursos.value
-    const novaEscolaB = new Senai(valorInputCidadeB, valorInputCodigoB, valorInputAnoB, valorInputCursosB)
+    const novaEscolaB = new Senai(valorInputCodigoB, valorInputCidadeB, valorInputAnoB, valorInputCursosB)
+    // Passando esse objeto para o escopo global
+    globalThis.novaEscolaB = novaEscolaB
     pMensagemB.textContent = `SENAI ${novaEscolaB.cidade} foi instanciada`
+  } else {
+    pMensagemB.textContent = 'Preencha todos os campos para criar a escola.'
   }
 
 })
@@ -98,30 +130,57 @@ btnInstanciarEscolaB.addEventListener('click', () => {
 /* ===== ABRIR ESCOLA A ===== */
 
 btnAbrirEscolaA.addEventListener('click', () => {
-  if (novaEscolaA.abrirEscola()){
-    pMensagemA.textContent = `Aberta: Bem-vindos ao SENAI ${novaEscolaA.cidade}`
+  if (validarCampos('A') === true) {
+  novaEscolaA.abrirEscola()
+  pMensagemA.textContent = `Aberta: Bem-vindos ao SENAI ${novaEscolaA.cidade}`
   } else {
-    pMensagemA.textContent = 'Não foi possivel abrir a escola'
+    pMensagemA.textContent = 'Preencha todos os campos para abrir a escola.'
   }
 })
 
 /* ===== ABRIR ESCOLA B ===== */
 
 btnAbrirEscolaB.addEventListener('click', () => {
-  if (novaEscolaB.abrirEscola()){
-    pMensagemA.textContent = `Aberta: Bem-vindos ao SENAI ${novaEscolaB.cidade}`
+  if (validarCampos('B') === true) {
+  novaEscolaB.abrirEscola()
+  pMensagemB.textContent = `Aberta: Bem-vindos ao SENAI ${novaEscolaB.cidade}`
   } else {
-    pMensagemA.textContent = 'Não foi possivel abrir a escola'
+    pMensagemB.textContent = 'Preencha todos os campos para abrir a escola.'
   }
 })
-
-/* ===== FECHAR ESCOLA ===== */
-
 
 
 /* ===== RELATÓRIO DE COMPARAÇÃO ===== */
 
+function detalhesRelatorio(){
+  if (validarCampos('A') === true || validarCampos('B') === true){
+    campoDetalhesA.textContent = `\nUNIDADE A \n Código ${novaEscolaA.codigo} \n Fundação ${novaEscolaA.anoConstrucao} (${novaEscolaA.idade}) \n Oferta ${novaEscolaA.qtdeCursos}`
+    campoDetalhesB.textContent = `\nUNIDADE B \n Código ${novaEscolaB.codigo} \n Fundação ${novaEscolaB.anoConstrucao} (${novaEscolaB.idade}) \n Oferta ${novaEscolaB.qtdeCursos}`
 
+  }
+}
+
+function compararDados(){
+  const relatorio = {
+    maisCursos: null,
+    menosCursos: null,
+    cursosIguais: false
+  }
+  
+  if (novaEscolaA.qtdeCursos > novaEscolaB.qtdeCursos){
+    relatorio.maisCursos = novaEscolaA
+    relatorio.menosCursos = novaEscolaB
+  }
+  else if(novaEscolaA.qtdeCursos < novaEscolaB.qtdeCursos){
+    relatorio.menosCursos = novaEscolaA
+    relatorio.maisCursos = novaEscolaB
+    
+  }
+  else if(novaEscolaA.qtdeCursos = novaEscolaB.qtdeCursos){
+    relatorio.cursosIguais = true
+    campoVeredito.textContent = 'As duas unidades são equivalentes aos cursos.'
+  }
+}
 
 /* ===== NOVA CONSULTA ===== */
 
